60031c2be3f49290dad81e95e5660922cd925872
cheyinbo <1403277945@qq.com>
2021-01-05 16:19:50 +0800

    IDEA整合Github测试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,11 @@
 package com.example.demo;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.Usermapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.awt.*;
-import java.sql.SQLException;
-import java.sql.Wrapper;
-import java.util.List;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -20,10 +15,14 @@ class DemoApplicationTests {
 
     @Test
     public void testFindAll(){
-        List<User> users = usermapper.selectList(null);
-        for (User user:users) {
-            System.out.println(user);
-        }
+//        List<User> users = usermapper.selectList(null);
+//        for (User user:users) {
+//            System.out.println(user);
+//        }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("name","Jone");
+        User user = usermapper.selectOne(queryWrapper);
+        System.out.println(user);
 
     }
 
