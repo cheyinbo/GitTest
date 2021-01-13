c15d7856893227f623159e172517af1e72290ee1
cheyinbo <1403277945@qq.com>
2021-01-13 16:29:00 +0800

    activeMQ消息队列测试(原生代码)

--- src/test/java/com/example/demo/ActiveMQTest02.java
+++ src/test/java/com/example/demo/ActiveMQTest02.java
@@ -14,13 +14,23 @@ public class ActiveMQTest02 {
 
 
     public static void main(String[] args) throws JMSException {
+        //实例化工厂
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQTest02.USERNAME,ActiveMQTest02.PASSWORD,ActiveMQTest02.BROKEURL);
+        //通过连接工厂来获取连接
         Connection connection = connectionFactory.createConnection();
+        //启动连接
+        connection.start();
+        //创建session,第一个参数为是否开启事务
         Session session = connection.createSession(Boolean.TRUE,Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue("CheQueue");
+        //创建消息队列
+        Destination destination = session.createQueue("CheQueue11");
+        //创建消息生产者
         MessageProducer messageProducer = session.createProducer(destination);
-        TextMessage textMessage = session.createTextMessage("这是CheQueue发送的队列消息");
+        //发送消息
+        TextMessage textMessage = session.createTextMessage("这是CheQueue发送的队列消息1111");
         messageProducer.send(textMessage);
+
+        session.commit();
         connection.close();
     }
 
